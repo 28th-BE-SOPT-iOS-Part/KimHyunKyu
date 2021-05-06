7d06eae5ada25c14c92275223f5b9a4abe0227a5
hyungyu Kim <ypsophila97@icloud.com>
2021-05-06 14:58:22 +0900

    [#9] week4-assignment
    - tableView 구현
    - cell 에서 화면전환할 때 데이터 전달
    
    close #9

---
+++ assignment1-cloneCoding-Kakao/assignment1-cloneCoding-Kakao/Resource/Storyboards/Cells/MyProfileCell.swift
@@ -0,0 +1,40 @@
+//
+//  MyProfileCell.swift
+//  assignment1-cloneCoding-Kakao
+//
+//  Created by kimhyungyu on 2021/05/05.
+//
+
+import UIKit
+
+class MyProfileCell: UITableViewCell {
+
+    //MARK: - Properties
+    
+    static let identifier = "MyProfileCell"
+    
+    //MARK: - @IBOutlet Properties
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setData(image: UIImage,
+                name: String,
+                state: String) {
+        profileImage.image = image
+        nameLabel.text = name
+        stateLabel.text = state
+    }
+}
--- assignment1-cloneCoding-Kakao/assignment1-cloneCoding-Kakao/Source/VCs/TabBarVCS/FirstTabVC.swift
+++ assignment1-cloneCoding-Kakao/assignment1-cloneCoding-Kakao/Source/VCs/TabBarVCS/FirstTabVC.swift
@@ -9,24 +9,152 @@ import UIKit
 
 class FirstTabVC: UIViewController {
     
+    //MARK: - Properties
+    
+    private var friendList:[FriendDataModel] = []
+    private var myProfile:FriendDataModel = FriendDataModel(imageName: "friendtabProfileImg", name: "김솝트", state: "상태메시지는 여기에")
+    
+    //MARK: - @IBOutlet Properties
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var friendLabel: UILabel!
+    
+    
     //MARK: - View Life Cycle
     
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        registerCell()
+        
+        setFriendList()
+        
+        setUI()
     }
     
-    //MARK: - @IBAction Properties
+    func setFriendList()
+        {
+            friendList.append(contentsOf: [
+                FriendDataModel(imageName: "profileImage1",
+                                name: "안솝트",
+                                state: "배고파요"),
+                
+                FriendDataModel(imageName: "profileImage2",
+                                name: "최솝트",
+                                state: "피곤해요"),
+                
+                FriendDataModel(imageName: "profileImage3",
+                                name: "정솝트",
+                                state: "시험언제끝나죠?"),
+                
+                FriendDataModel(imageName: "profileImage4",
+                                name: "이솝트",
+                                state: "ㅠㅠㅠㅠ"),
+                
+                FriendDataModel(imageName: "profileImage5",
+                                name: "유솝트",
+                                state: "나는 상태메세지!"),
+                
+                FriendDataModel(imageName: "profileImage6",
+                                name: "박솝트",
+                                state: "원하는대로 바꿔보세요 ^_^"),
+                
+                FriendDataModel(imageName: "profileImage7",
+                                name: "최솝트",
+                                state: "넘 덥다.."),
+                
+                FriendDataModel(imageName: "profileImage8",
+                                name: "원솝트",
+                                state: "배고파요"),
+                
+                FriendDataModel(imageName: "profileImage9",
+                                name: "투솝트",
+                                state: "내꿈은 대나무부자"),
+                
+                FriendDataModel(imageName: "profileImage10",
+                                name: "권솝트",
+                                state: "걱정말라구!")
+            ])
+        }
+    
+    private func setUI() {
+        friendLabel.text = "친구"
+        friendLabel.font = UIFont.boldSystemFont(ofSize: 22)
+        tableView.separatorStyle = .none
+    }
+    
+    private func registerCell() {
+        let myProfileCell = UINib(nibName: "MyProfileCell", bundle: nil)
+        tableView.register(myProfileCell, forCellReuseIdentifier: MyProfileCell.identifier)
+        
+        let friendCell = UINib(nibName: "FriendCell", bundle: nil)
+        tableView.register(friendCell, forCellReuseIdentifier: FriendCell.identifier)
+    }
+}
 
-    @IBAction func touchProfilImageView(_ sender: Any) {
+//MARK: - UITableViewDelegates
+
+extension FirstTabVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let myTabVC = UIStoryboard.init(name: "MyTab", bundle: nil)
         guard let nextVC = myTabVC.instantiateViewController(identifier: "MyTabVC") as? MyTabVC else {
             return
         }
         //overFullScreen, overCurrentContext 는 반투명도를 조절해서 뒤의 view 를 볼 수 있다.
         nextVC.modalPresentationStyle = .overFullScreen
-        nextVC.modalTransitionStyle = .coverVertical
         nextVC.view.alpha = 1
+        
+        if indexPath.row == 0 {
+            nextVC.nameLabel.text = myProfile.name
+            nextVC.profileImageView.image = myProfile.image
+        } else {
+            nextVC.nameLabel.text = friendList[indexPath.row-1].name
+            nextVC.profileImageView.image = friendList[indexPath.row-1].image
+        }
+        
         self.present(nextVC, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 73
+        } else {
+            return 50
+        }
     }
 }
 
+//MARK: - UITableViewDataSources
+
+extension FirstTabVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 11
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            guard let myProfileCell = tableView.dequeueReusableCell(withIdentifier: MyProfileCell.identifier, for: indexPath) as? MyProfileCell else {
+                return UITableViewCell()
+            }
+            myProfileCell.setData(image: myProfile.image,
+                              name: myProfile.name,
+                              state: myProfile.state)
+
+            return myProfileCell
+        } else {
+            guard let friendCell = tableView.dequeueReusableCell(withIdentifier: FriendCell.identifier, for: indexPath) as? FriendCell else {
+                return UITableViewCell()
+            }
+            friendCell.setData(image: friendList[indexPath.row-1].image,
+                               name: friendList[indexPath.row-1].name,
+                               state: friendList[indexPath.row-1].state)
+            
+            return friendCell
+        }
+    }
+    
+    
+    
+}
